e1b7d3c86a7655b0cdff4a81b82ad38b05a79652
Moisés Dutra Teixeira <moisesdutrateixeira@gmail.com>
2026-01-08 11:37:45 -0300

    Delete EXERCICIOS-PYTHON directory

--- EXERCICIOS-PYTHON/exercicio_1.py
+++
@@ -1,9 +0,0 @@
-#Printe a frase 'meu nome é {nome} e tenho {idade} anos' colocando nome e idade
-#em variaveis e depois printe 'ALURA' com uma letra em cada linha.
-
-nome = 'Moisés'
-idade = 20
-
-print(f'Meu nome é {nome} e tenho {idade} anos')
-
-print('A\nL\nU\nR\nA')
