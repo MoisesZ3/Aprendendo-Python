61cd2d3b65f85b7468df626ed39a5f9142f7f8e1
Moisés Dutra Teixeira <moisesdutrateixeira@gmail.com>
2026-01-12 11:55:12 -0300

    Add files via upload

--- PYTHON-APLICANDO-ORIENTAÇÃO-OBJETO/OO-SABOR-EXPRESS/Modelos/restaurante.py
+++ PYTHON-APLICANDO-ORIENTAÇÃO-OBJETO/OO-SABOR-EXPRESS/Modelos/restaurante.py
@@ -2,17 +2,26 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self,nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria} | {self.ativo}'
+        return f'{self._nome} | {self._categoria} | {self._ativo}'
     
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
 
 restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_pizza = Restaurante('Pizza Express','Italiana')
