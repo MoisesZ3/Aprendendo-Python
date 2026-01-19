af80adb88c66f28bcabdf9bab1e8d1bd371031e0
Moisés Dutra Teixeira <moisesdutrateixeira@gmail.com>
2026-01-18 21:36:00 -0300

    Add files via upload

---
+++ AVANCE_NA_ORIENTACAO_OBJETO_E_CONSUMA_API/EXERCICIOS/exercicios/moto.py
@@ -0,0 +1,9 @@
+from exercicios.veiculo import Veiculo
+
+class Moto(Veiculo):
+    def __init__(self,marca,modelo,tipo):
+        super().__init__(marca,modelo)
+        self._tipo = tipo
+
+    def __str__(self):
+        return f'{self._marca} | {self._modelo} | {self._tipo} | {self._ligado}'
---
+++ AVANCE_NA_ORIENTACAO_OBJETO_E_CONSUMA_API/EXERCICIOS/main.py
@@ -0,0 +1,22 @@
+from exercicios.carro import Carro
+from exercicios.moto import Moto
+
+carro_1 = Carro('Fiat','Mob',4)
+carro_2 = Carro('Hyundai','Veloster',3) 
+carro_3 = Carro('Jeep','Wrangler',2)
+
+moto_1 = Moto('Kawasaki','Ninja ZX-10R','Esportiva')
+moto_2 = Moto('Yamaha','YZF-R15','Esportiva')
+moto_3 = Moto('Royal Enfield','Classic 350','Casual')
+
+def __main__():
+    print(carro_1)
+    print(carro_2)
+    print(carro_3)
+
+    print(moto_1)
+    print(moto_2)
+    print(moto_3)
+    
+if __name__ == '__main__':
+    __main__()
